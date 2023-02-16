41487bc127f748bdc8395d2c8393cf93f4ad5d05
Biri <test@email>
2023-02-16 17:16:38 +0100

    INCONV-3 [+] update/ remove from cart added as gateaway

--- src/main/java/com/bitsmilez/authentificationmicroservice/core/service/impl/Gateway.java
+++ src/main/java/com/bitsmilez/authentificationmicroservice/core/service/impl/Gateway.java
@@ -1,7 +1,6 @@
 package com.bitsmilez.authentificationmicroservice.core.service.impl;
 
 import com.bitsmilez.authentificationmicroservice.core.service.interfaces.IGateway;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
@@ -22,7 +21,7 @@ public class Gateway implements IGateway {
         String json = jsonObject.toString();
         RequestBody body = RequestBody.create(MediaType.parse("application/json"),json);
 
-        String url = "http://localhost:8080/add-to-cart";
+        String url = "http://localhost:8080/cart/add-to-cart";
         OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder()
@@ -42,12 +41,58 @@ public class Gateway implements IGateway {
     }
 
     @Override
-    public ResponseEntity<?> publishRemoveFromCartEvent(ObjectNode objectNode) {
-        return null;
+    public ResponseEntity<?> publishRemoveFromCartEvent(String productID, String cartID) throws IOException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.accumulate("productID", productID);
+        jsonObject.accumulate("cartID",cartID);
+        String json = jsonObject.toString();
+        RequestBody body = RequestBody.create(MediaType.parse("application/json"),json);
+
+        String url = "http://localhost:8080/cart/remove-from-cart";
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        String response = client.newCall(request).execute().toString();
+        int codeIndex = response.indexOf("code="); // find the index of "code=" in the string
+        int commaIndex = response.indexOf(",", codeIndex); // find the index of the next comma after "code="
+
+        String codeStr = response.substring(codeIndex + 5, commaIndex); // extract the substring that represents the code number
+        int code = Integer.parseInt(codeStr); // convert the string to an integer
+        return  new ResponseEntity<>(HttpStatus.valueOf(code));
     }
 
     @Override
-    public ResponseEntity<?> publishUpdateCartEvent(ObjectNode objectNode) {
-        return null;
+    public ResponseEntity<?> publishUpdateCartEvent(String productID, int quantity,String cartID) throws IOException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.accumulate("productID", productID);
+        jsonObject.accumulate("cartID",cartID);
+        jsonObject.accumulate(("quantity"),quantity);
+        String json = jsonObject.toString();
+        RequestBody body = RequestBody.create(MediaType.parse("application/json"),json);
+
+        String url = "http://localhost:8080/cart/update-cart";
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        String response = client.newCall(request).execute().toString();
+        int codeIndex = response.indexOf("code="); // find the index of "code=" in the string
+        int commaIndex = response.indexOf(",", codeIndex); // find the index of the next comma after "code="
+
+        String codeStr = response.substring(codeIndex + 5, commaIndex); // extract the substring that represents the code number
+        int code = Integer.parseInt(codeStr); // convert the string to an integer
+        return  new ResponseEntity<>(HttpStatus.valueOf(code));
+
     }
+
+
 }
--- src/main/java/com/bitsmilez/authentificationmicroservice/core/service/interfaces/IGateway.java
+++ src/main/java/com/bitsmilez/authentificationmicroservice/core/service/interfaces/IGateway.java
@@ -1,14 +1,13 @@
 package com.bitsmilez.authentificationmicroservice.core.service.interfaces;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.IOException;
 
 public interface IGateway {
 
     ResponseEntity<?> publishAddToCartEvent(String productID, Integer amount) throws IOException;
-    ResponseEntity<?> publishRemoveFromCartEvent(@RequestBody ObjectNode objectNode);
-    ResponseEntity<?> publishUpdateCartEvent(@RequestBody ObjectNode objectNode);
+    ResponseEntity<?> publishRemoveFromCartEvent(String productID, String cartID) throws IOException;
+
+    ResponseEntity<?> publishUpdateCartEvent(String productID,int quantity, String cartID) throws IOException;
 }
