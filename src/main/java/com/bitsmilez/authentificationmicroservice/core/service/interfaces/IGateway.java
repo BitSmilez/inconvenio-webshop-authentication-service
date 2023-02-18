36491399b1d51dbb192748565d9f40af40ef7537
jutnh <jutnhbr@gmail.com>
2023-02-18 16:29:32 +0100

    INCONV-3 [+] Added missing cartID the Request

--- src/main/java/com/bitsmilez/authentificationmicroservice/core/service/interfaces/IGateway.java
+++ src/main/java/com/bitsmilez/authentificationmicroservice/core/service/interfaces/IGateway.java
@@ -6,8 +6,7 @@ import java.io.IOException;
 
 public interface IGateway {
 
-    ResponseEntity<?> publishAddToCartEvent(String productID, Integer amount) throws IOException;
+    ResponseEntity<?> publishAddToCartEvent(String productID, Integer amount, String userID) throws IOException;
     ResponseEntity<?> publishRemoveFromCartEvent(String productID, String cartID) throws IOException;
-
     ResponseEntity<?> publishUpdateCartEvent(String productID,int quantity, String cartID) throws IOException;
 }
