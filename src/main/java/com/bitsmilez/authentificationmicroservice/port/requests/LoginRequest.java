08f9a92e34fa383e37d14c259a1df52f89c32a54
jutnh <jutnhbr@gmail.com>
2023-03-12 22:25:52 +0100

    INCONV-73 [+] Added Setter for testing

--- src/main/java/com/bitsmilez/authentificationmicroservice/port/requests/LoginRequest.java
+++ src/main/java/com/bitsmilez/authentificationmicroservice/port/requests/LoginRequest.java
@@ -1,8 +1,10 @@
 package com.bitsmilez.authentificationmicroservice.port.requests;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class LoginRequest {
 
     String username;
